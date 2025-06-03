6dcec72d65177eab4b2bae0b650321ba4113ae93
Erdos Balint <valerauko@gmail.com>
2025-06-03 16:11:05 +0900

    Add repo infra

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject jp.studist/annotation-reporter "0.1.0"
+  :description "GitHub annotation test reporter for Kaocha"
+  :url "https://github.com/StudistCorporation/annotation-reporter"
+  :license {:name "MIT"
+            :url "https://opensource.org/license/mit/"}
+  :dependencies []
+  :scm {:name "git"
+        :tag "HEAD"}
+  :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
+                                   :username :env/clojars_user
+                                   :password :env/clojars_token}}
+  :profiles
+  {:dev {:dependencies [[clj-kondo "2025.04.07"]
+                        [org.clojure/clojure "1.12.0"]
+                        [lambdaisland/kaocha "1.91.1392"]]
+         :plugins [[lein-ancient "0.7.0"]]
+         :aliases {"lint" ["run" "-m" "clj-kondo.main"
+                           "--config" ".clj-kondo/config.edn"
+                           "--lint" "src" "test"]
+                   "test" ["run" "-m" "kaocha.runner"]}}})
